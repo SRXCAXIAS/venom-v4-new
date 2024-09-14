6b37eaf50ef52b40251dcd3a558caf32b98af71d
SRXCAXIAS <manashirath32@gmail.com>
2024-09-14 13:38:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 BOT_TOKEN=("7526715161:AAEF7UqRws12hPZn2kcpt24LXUoWT7s-Tm0")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("sibuna")
+OWNER_USERNAME=("TC sanatan")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("7186024164")
